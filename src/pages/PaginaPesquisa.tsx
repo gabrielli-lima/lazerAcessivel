00bba45754d22a62a05634a86b0499f15d757014
gabrielli-lima <gabriellilima165@gmail.com>
2023-09-18 22:57:23 -0300

    feat: adicionando rota para a pagina de pesquisa

---
+++ src/pages/PaginaPesquisa.tsx
@@ -0,0 +1,10 @@
+
+export const PaginaPesquisa = () => {
+return(
+    <div className='absolute inset-0 scroll-smooth flex flex-row'>
+
+        <div className="bg-orange-200 w-2/5 h-full"></div>
+        <div className="bg-green-500 w-3/5 h-full"></div>
+    </div>
+)
+}
