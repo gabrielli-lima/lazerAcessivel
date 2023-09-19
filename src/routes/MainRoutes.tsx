f6629fe3fcc8e6c553ac2b1fa63fbe77ac22a829
gabrielli-lima <gabriellilima165@gmail.com>
2023-09-18 23:14:02 -0300

    bug: redirecionando rotas

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -3,9 +3,10 @@ import { PaginaPesquisa } from '../pages/PaginaPesquisa'
 import { Home } from '../pages/Home'
 
 export const RouterList = () => {
+    const base = 'lazerAcessivel'
     return useRoutes([
-        { path: '/', element: <Home/> },
-        { path: '/pesquisar', element: <PaginaPesquisa /> }
+        { path: `/${base}`, element: <Home/> },
+        { path: `/${base}/pesquisar`, element: <PaginaPesquisa /> }
         // { path: '*', element: <NotFound /> },
 
     ])
